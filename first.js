3994c02cde3d042fe51e5fb74484acd4450e4b9d
Nehacomp2021 <79760215+Nehacomp2021@users.noreply.github.com>
2021-02-28 00:10:32 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,7 @@
+function changecolor(){
+    document.getElementById("h1").style.color="red";
+}
+
+function setDate(){
+    document.getElementById('date').innerHTML = new Date().toDateString();
+}
